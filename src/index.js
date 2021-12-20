1504a3a444fc57119b0268b3d69727d5289e3d91
Vivian Yim <keroviv86@gmail.com>
2021-12-19 22:05:34 -0600

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const todoForm= document.getElementById("create-task-form")
+    todoForm.addEventListener('submit', handleSubmit)
+  
 });
+
+
+function handleSubmit(event){
+  event.preventDefault();
+  console.log('submitted')
+  let todo = event.target["new-task-description"].value
+  console.log(todo)
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+
+
+  li.textContent = todo
+  console.log(li)
+
+  btn.addEventListener('click', ()=> {
+    li.remove()
+  })
+  btn.textContent = 'X'
+  li.appendChild(btn)
+
+  document.querySelector('#tasks').append(li)
+  event.target.reset()
+}
